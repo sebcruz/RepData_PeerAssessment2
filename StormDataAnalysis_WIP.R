61b5d2095d1913f513db2f236ee92fa874bfaa0a
Myrmidon010 <sebcruz@outlook.com>
2015-04-25 17:37:49 -0700

    Update2

--- StormDataAnalysis_WIP.R
+++ StormDataAnalysis_WIP.R
@@ -18,13 +18,22 @@ SchoolData$Grade<-
   recode(SchoolData$Grade,
          "c(1,2,3,4,5) ='Five or Less'")
 
+SchoolData$Grade<-
+  recode(SchoolData$Grade,"
+    1:5='Elementary';
+    6:8='Middle;
+    else='High'
+  ")
+
+recodetest = as.data.frame(unique(data.raw$PROPDMGEXP))
+colnames(recodetest) = c("PROPDMGEXP")
+
 data.raw$modPROPDMGEXP = as.numeric(
-  recode(data.raw, "
-    c('-', '?', '+', '') = 0;
-    c('h', 'H') = 1e+02;
-    c('k', 'K') = 1e+03;
-    c('m', 'M') = 1e+06;
-    c('b', 'B') = 1e+09;
+  recode(data.raw$PROPDMGEXP, "
+    c('h','H') = 1e+02;
+    c('k','K') = 1e+03;
+    c('m','M') = 1e+06;
+    c('b','B') = 1e+09;
     '0' = 1; 
     '1' = 10; 
     '2' = 1e+02; 
@@ -33,16 +42,23 @@ data.raw$modPROPDMGEXP = as.numeric(
     '5' = 1e+05;
     '6' = 1e+06; 
     '7' = 1e+07; 
-    '8' = 1e+08;", as.factor.result = FALSE
+    '8' = 1e+08;
+    '-' = 0;
+    '?' = 0;
+    '+' = 0;
+    '' = 0;
+    ", as.factor.result = FALSE
   )
 )
 
-data.raw$modPROPDMGEXP = as.numeric(
-  recode(data.raw, "
-         c('-', '?', '+', '') = 0;
-", as.factor.result = FALSE
+
+
+recodetest$modPROPDMGEXP = as.numeric(
+  recode(recodetest$PROPDMGEXP, "
+    c('h','H') = 1e+02;
+  ", as.factor.result = FALSE
   )
-  )
+)
 
 
 data.raw$mod2PROPDMGEXP = as.numeric(
